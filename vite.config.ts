bcc54ab23a7dc8d4ad5c573e7ad2de5681b60eb2
kanglouis <doanhuu.lehuan@gmail.com>
2025-10-20 14:58:17 +0700

    Update package manager and add pnpm lockfile; configure Vite build options and update HTML structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   plugins: [
     react(),
